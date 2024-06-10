cfca3af9be88eb6df2a7e45cd8186f894fe928fc
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-06-10 15:23:11 +0700

    add: prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import newsRouter from './news.route.js';
 import siteRouter from './site.route.js';
 
 const route = (app) => {
-	app.use('/news', newsRouter); app.use('/', siteRouter);
+  app.use('/news', newsRouter);
+  app.use('/', siteRouter);
 };
 export default route;
